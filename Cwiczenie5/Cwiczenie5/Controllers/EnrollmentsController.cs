8b4017c708fd384862149c27d2d73e9a4cad9302
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-12-26 22:48:11 +0100

    Wstawiony zostaje do tabeli Enrollment wiersz jeśli nie istnieje

--- Cwiczenie5/Cwiczenie5/Controllers/EnrollmentsController.cs
+++ Cwiczenie5/Cwiczenie5/Controllers/EnrollmentsController.cs
@@ -37,35 +37,114 @@ namespace Cwiczenie5.Controllers
             {
                 com.Connection = con;
 
-
                 com.CommandText = " select IdStudy from Studies where Name Like @name ";
-
                 com.Parameters.AddWithValue("name", request.Studies);
+
                 con.Open();
                 var transacion = con.BeginTransaction();
+
                 com.Transaction = transacion;
-                using (SqlDataReader dr = com.ExecuteReader()) { 
+
+                SqlDataReader dr = com.ExecuteReader();
+                if (!dr.Read())
+                {
+                    dr.Close();
+                    transacion.Rollback();
+                    return BadRequest("Studia nie istnieją");
+                }
+
+                int IdStudy = (int)dr["IdStudy"];
+                dr.Close();
 
 
 
-                    if (!dr.Read())
-                    {
-                        dr.Close();
-                        transacion.Rollback();
-                        return BadRequest("nie znaleziono studiów");
+                com.CommandText = "Select IdEnrollment from Enrollment where Semester=1 and IdStudy= (select IdStudy from Studies where Name Like @name) order by StartDate DESC ";
+                com.Transaction = transacion;
+
+                dr = com.ExecuteReader();
+                int IdEnrollment = 1;
+
+                if (dr.Read())
+                {
+                    IdEnrollment = (int)dr["IdEnrollment"];
+                    dr.Close();
+
+                }else if (!dr.Read())
+                {
+                    dr.Close();
+                    com.CommandText = "select IdEnrollment from Enrollment where IdEnrollment =(select max(IdEnrollment) from Enrollment )";
+                    dr = com.ExecuteReader();
+                    if (dr.Read())
+                    {int IdEnroll = (int)dr["IdEnrollment"];
+                        IdEnrollment = IdEnroll + 1;
 
 
                     }
-               var response = new EnrollStudentResponse();
-            response.IndexNumber = student.IndexNumber;
-            //  response.Semester = ;
-            //  response.StartDate =/
+                        
+                        dr.Close();
+
+                    com.CommandText = "INSERT INTO Enrollment(IdEnrollment, Semester, IdStudy, StartDate) VALUES (@IdEnrollment , 1, @IdStudy,  convert(datetime, @StartDate));";
+
+                    com.Parameters.AddWithValue("IdStudy", IdStudy);
+                    DateTime thisDay = DateTime.Today;
+                    com.Parameters.AddWithValue("StartDate", thisDay);
+                  com.Parameters.AddWithValue("IdEnrollment", IdEnrollment);
+
+                    
+                    com.ExecuteNonQuery();
+                    transacion.Commit();
+                    
+                }
 
 
-            return Ok(response);}
+
+            }
+
+
+        
+
+
+
+
+
+
+
+                //zapisujemy teraz studenta 
+
+
+
+
+
+
+
+
+                // jeśli studia istnieją najnowszy wpis w tabeli enrollments zgodny ze studiami i wartościa semestru=1
+                //zapisujemy studenta na semestr pierwszy
+                // jesli wpisu nie ma to dodajemy go do bazy i ustawiamy start na aktualną dat e 
+
+
+
+
+
+
+                //sprawdzamy index studenta czy unikalny
+                //jesli nei zgłaszamy błąd 
+
+                //jeśli tak dodajemy studenta
+
+           
+
+                var response = new EnrollStudentResponse();
+                    response.IndexNumber = student.IndexNumber;
+                    //  response.Semester = ;
+                    //  response.StartDate =/
+
+
+                    return Ok(response);
+                }
              }
             
-        }
+        
 
 
 
@@ -92,4 +171,4 @@ namespace Cwiczenie5.Controllers
 
 
     }
-}
+
