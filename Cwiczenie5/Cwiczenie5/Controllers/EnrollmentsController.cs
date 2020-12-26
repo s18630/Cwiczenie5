045403ee859f9f16bf20f9745ac40a952fb113be
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-12-26 14:58:13 +0100

    Początek Reader zadziałał

---
+++ Cwiczenie5/Cwiczenie5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cwiczenie5.DTOs.Requests;
+using Cwiczenie5.DTOs.Responses;
+using Cwiczenie5.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cwiczenie5.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        private const string ConString = "Data Source=db-mssql;Initial Catalog=2019SBD;Integrated Security=True";
+
+
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+
+            var student = new Student();
+            student.IndexNumber = request.IndexNumber;
+            student.FirstName = request.FirstName;
+            student.LastName = request.LastName;
+            student.BirthDate = request.BirthDate;
+
+
+
+
+
+            using (SqlConnection con = new SqlConnection(ConString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con; 
+
+                com.CommandText = " select IdStudy from Studies where Name Like @name ";
+
+                com.Parameters.AddWithValue("name", request.Studies);
+                con.Open();
+
+                using (SqlDataReader dr=com.ExecuteReader())
+                {
+                    if (!dr.Read())
+                    {
+                        return BadRequest("nie znaleziono studiów");
+
+                    }
+               var response = new EnrollStudentResponse();
+            response.IndexNumber = student.IndexNumber;
+            //  response.Semester = ;
+            //  response.StartDate =/
+
+
+            return Ok(response);
+            } }
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Cwiczenie5/Cwiczenie5/DTOs/Responses/EnrollStudentResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cwiczenie5.DTOs.Responses
+{
+    public class EnrollStudentResponse
+    {
+        public string IndexNumber { get; set; }
+ 
+        public string Semester { get; set; }
+
+        public string StartDate { get; set; }
+    }
+}
